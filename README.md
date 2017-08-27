9f273cfb659a1bf3f7d8eca0acc8e5bd454b8ecc
Daksh Gupta <dakshkumargupta@gmail.com>
2017-08-27 10:40:31 -0500

    Added bfs with doctests; updated readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Webley Cosing Puzzle
+# Webley Coding Puzzle
 
 **By:** Daksh Gupta
 
@@ -9,6 +9,29 @@
 Doctests are included in the methods, to ensure that the code is working as expected.
 
 They can be executed using the following command:
-```batch
+```shell
 python -m doctest -v find_order.py
 ```
+
+### Execution
+
+To run the program use the following command on your console:
+```shell
+python find_order.py [OPTIONS]
+```
+
+Available options:
+
+Option | Description
+--- | ---
+`-h` | Help
+`-t`, `--target` | Specify CSV file with target value. Defaults to 'target.csv'
+`-d`, `--data` | Specify CSV file with menu data. Defaults to 'data.csv'
+
+The target file should contain data as follows
+```
+target,$15.05
+```
+The value `$15.05` can be anything as long as it is specified to a precision of 2 decimal places and begins with '$'.
+
+All menu items should also be specified in the same format in a separate CSV file.
--- find_order.py
+++ find_order.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import sys
+import itertools
 
 
 def match_price_pattern(price):
@@ -39,24 +40,106 @@ def match_price_pattern(price):
     pattern = re.compile('\$[0-9]+\.[0-9]{2}')
     return True if pattern.fullmatch(price) else False
 
+
+def parse_data_file(lines):
+    """
+    This function parses the list of lines in data file containing the menu
+    items and returns a dictionary with price as keys and item names as values.
+
+    >>> parse_data_file(['acb,123', 'amc,asd'])
+    {}
+    >>> {'abc':1.0,'ans':30.0}==parse_data_file(['abc,$1.00\\n','ans,$30.00\\n'])
+    True
+    >>> {'abc':1.0,'anc':30.01}==parse_data_file(['abc , $1.00\\n','   anc, $30.01\\n'])
+    True
+    """
+    menu_items = {}
+    for line in lines:
+        item = None
+        price = None
+        try:
+            line = line.strip()
+            item, price = line.split(',')
+        except Exception as e:
+            print('Invalid format price format, {}, for data file ...'.format(
+                price
+                ))
+            print('\nAccepted format: $xxxx.xx')
+            print('Examples: $15.00, $1.00, $0.03')
+            print('\nExiting ...')
+            return {}
+        else:
+            item = item.strip()
+            price = price.strip()
+            if match_price_pattern(price):
+                menu_items[item] = float(price[1:])
+    return menu_items
+
+
+def breadth_first_search(target, menu):
+    """
+    This function performs breadth first graph search on the menu items
+    to find a combination of itmes whose price sums up to the target value.
+
+    >>> {'a','c','d'} == breadth_first_search(15, {'a':2,'b':4,'c':6,'d':7})
+    True
+    >>> {'c'} == breadth_first_search(6, {'a':2,'b':4,'c':6,'d':7})
+    True
+    >>> frozenset() == breadth_first_search(18, {'a':2,'b':4,'c':6,'d':7})
+    True
+    """
+    visited = set()
+    queue = [(frozenset([item]), menu[item]) for item in menu]
+    while queue:
+        combination, cur_sum = queue.pop(0)
+        if combination not in visited:
+            visited.add(combination)
+            if cur_sum < target:
+                for item, price in menu.items():
+                    new_combination = combination | frozenset([item])
+                    if new_combination not in visited:
+                        queue.append((new_combination, cur_sum+price))
+            if cur_sum == target:
+                return combination
+        # print(visited)
+    return frozenset()
+
+
+def find_combination(target, menu):
+    """
+    This function filters the menu items to only contain items that have
+    price less than or equal to the target price and feed it to
+    breadth_first_search.
+
+    Returns: A frozenset containing combination of menu items.
+             An empty frozenset if no combination is possible.
+    """
+    filtered_menu = dict([(key, value) for key, value in menu.items() if value <= target])
+    return breadth_first_search(target, menu)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-t',
         '--target',
-        help='Location of CSV file containing target price.',
+        help='''Location of CSV file containing target price.
+        Defaults to 'target.csv'.
+        ''',
         nargs='?',
-        default='target.csv',
         const='target.csv',
+        required=True,
         )
 
     parser.add_argument(
         '-d',
         '--data',
-        help='Location of CSV file containing menu data.',
+        help='''Location of CSV file containing menu data.
+        Defaults to 'data.csv'.
+        ''',
         nargs='?',
-        default='data.csv',
         const='data.csv',
+        required=True,
         )
 
     args = parser.parse_args()
@@ -70,7 +153,7 @@ if __name__ == '__main__':
         print('Try with administrative priviledges.')
 
     _, target = target.readline().split(',')
-    if match_price_pattern(target):
+    if match_price_pattern(target.strip()):
         target = float(target[1:])
     else:
         print('Price data, {}, not in accepted format ...'.format(target))
@@ -79,4 +162,28 @@ if __name__ == '__main__':
         print('\nExiting ...')
         sys.exit()
 
-    print(target)
+    # print(target)
+    data_file = None
+    try:
+        data_file = open(args.data, 'r')
+    except FileNotFoundError as e:
+        print('Unable to find {} ...'.format(args.target))
+    except Exception as e:
+        print('Unable to open {} ...'.format(args.target))
+        print('Try with administrative priviledges.')
+
+    menu = parse_data_file(data_file)
+    if not menu:
+        sys.exit()
+
+    print('Target price: ${:.2f}'.format(target))
+    print('\nMenu:')
+    for item in menu:
+        print('{}\t${:.2f}'.format(item, menu[item]))
+    print()
+
+    combination = find_combination(target, menu)
+    if len(combination) == 0:
+        print('There is no combination of dishes that is equal to the target price')
+    else:
+        [print(i) for i in combination]
